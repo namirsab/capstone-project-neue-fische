f358857add0fb7e93bb06fe171ae1b7a4914f7b8
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-05-06 13:08:50 +0200

    Prof of concept: using the query string as the state for the filters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import SignInPage from "./components/SignInPage";
 import BookmarkPage from "./components/BookmarkPage";
@@ -12,13 +12,29 @@ import ViewPage from "./components/ViewPage";
 import OutdoorPage from "./components/OutdoorPage";
 import PartOfTownPage from "./components/PartOfTownPage";
 import RestaurantDetailedPage from "./components/RestaurantDetailedPage";
+import { parse } from "./utilities/queryString";
 
 function App() {
   const [restaurants, setRestaurants] = useState([]);
+  const location = useLocation();
+
+  const filters = parse(location.search);
+  /*
+    This is an example of how to deal with the filtering.
+    In your code, you should have all the filtering stuff in a
+    separate module, then import it here and use it 
+  */
+
+  const filteredRestaurants = restaurants.filter((restaurant) => {
+    if (filters.takeAway?.includes("delivery")) {
+      return restaurant.delivery === true;
+    }
+    return true;
+  });
 
   useEffect(() => {
     const fetchRestaurants = () => {
-      return fetch("restaurants.json")
+      return fetch("/restaurants.json")
         .then((response) => response.json())
         .then((restaurantData) => {
           const newData = restaurantData.results;
@@ -29,46 +45,44 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <SignInPage />
-          </Route>
-          <Route exact path="/bookmarkPage">
-            <BookmarkPage />
-          </Route>
-          <Route exact path="/takeAwayPage">
-            <TakeAwayPage restaurantData={restaurants} />
-          </Route>
-          <Route exact path="/openPage">
-            <OpenPage />
-          </Route>
-          <Route exact path="/ambiencePage">
-            <AmbiencePage />
-          </Route>
-          <Route exact path="/cuisinePage">
-            <CuisinePage />
-          </Route>
-          <Route exact path="/viewPage">
-            <ViewPage />
-          </Route>
-          <Route exact path="/outdoorPage">
-            <OutdoorPage />
-          </Route>
-          <Route exact path="/partOfTownPage">
-            <PartOfTownPage />
-          </Route>
-          <Route exact path="/resultsPage">
-            <ResultsPage restaurantData={restaurants} />
-          </Route>
-          <Route exact path="/restaurantDetailedPage/:id">
-            <RestaurantDetailedPage restaurantData={restaurants} />
-          </Route>
-          <Route exact path="/loadingPage"></Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <SignInPage />
+        </Route>
+        <Route exact path="/bookmarkPage">
+          <BookmarkPage />
+        </Route>
+        <Route exact path="/takeAwayPage">
+          <TakeAwayPage restaurantData={filteredRestaurants} />
+        </Route>
+        <Route exact path="/openPage">
+          <OpenPage />
+        </Route>
+        <Route exact path="/ambiencePage">
+          <AmbiencePage />
+        </Route>
+        <Route exact path="/cuisinePage">
+          <CuisinePage />
+        </Route>
+        <Route exact path="/viewPage">
+          <ViewPage />
+        </Route>
+        <Route exact path="/outdoorPage">
+          <OutdoorPage />
+        </Route>
+        <Route exact path="/partOfTownPage">
+          <PartOfTownPage />
+        </Route>
+        <Route exact path="/resultsPage">
+          <ResultsPage restaurantData={restaurants} />
+        </Route>
+        <Route exact path="/restaurantDetailedPage/:id">
+          <RestaurantDetailedPage restaurantData={restaurants} />
+        </Route>
+        <Route exact path="/loadingPage"></Route>
+      </Switch>
+    </div>
   );
 }
 
